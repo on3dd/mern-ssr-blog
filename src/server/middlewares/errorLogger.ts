7e3a73610d7c218832ff0582988537c91aa4d3b5
on3dd <onedeadwave@gmail.com>
2021-01-24 21:13:03 +1000

    minor errorLogger fix

--- src/server/middlewares/errorLogger.ts
+++ src/server/middlewares/errorLogger.ts
@@ -5,18 +5,22 @@ import ErrorHandler from '@server/shared/ErrorHandler';
 
 const errorHandler = ErrorHandler.getInstance();
 
+type Err = Error | string;
+
 /** Note: It must be placed after the router! */
 const errorLogger = (
-  err: Error,
+  err: Err,
   req: Request,
   res: Response,
   _: NextFunction,
 ) => {
-  errorHandler.handleError(err);
+  const error = err instanceof Error ? err : new Error(err);
+
+  errorHandler.handleError(error);
 
   return res.status(StatusCodes.BAD_REQUEST).json({
     data: null,
-    error: err.message,
+    error: error.message,
   });
 };
 
